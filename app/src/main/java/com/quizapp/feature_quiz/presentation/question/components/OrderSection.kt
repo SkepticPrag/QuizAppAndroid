b2fe049a335926de2c6b740d8d7ff17e7da5f6dc
Germán Lobbia <germanlobbia@gmail.com>
2023-05-14 23:41:54 -0300

    [W] OrderSection

--- app/src/main/java/com/quizapp/feature_quiz/presentation/question/components/OrderSection.kt
+++ app/src/main/java/com/quizapp/feature_quiz/presentation/question/components/OrderSection.kt
@@ -1,2 +1,24 @@
 package com.quizapp.feature_quiz.presentation.question.components
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.quizapp.feature_quiz.data.repository.QuestionRepositoryImplementation
+import com.quizapp.feature_quiz.domain.util.OrderType
+import com.quizapp.feature_quiz.domain.util.QuestionOrder
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+    questionOrder: QuestionOrder = QuestionOrder.Category(OrderType.Descending),
+    onOrderChange: (QuestionOrder)->Unit
+)
+{
+    Column (
+        modifier =  modifier
+    )
+    {
+
+    }
+}
