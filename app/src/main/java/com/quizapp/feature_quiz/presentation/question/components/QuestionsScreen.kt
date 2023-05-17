9d5c5cebe06b9f7f0f42a1470f70d5e7e927da43
Germán Lobbia <germanlobbia@gmail.com>
2023-05-16 23:26:20 -0300

    [F] Fixed questionscreen

--- app/src/main/java/com/quizapp/feature_quiz/presentation/question/components/QuestionsScreen.kt
+++ app/src/main/java/com/quizapp/feature_quiz/presentation/question/components/QuestionsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.*
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
