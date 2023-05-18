c7ddc5b036ae78e6ee2e87bcd2b88b342b175064
Germán Lobbia <germanlobbia@gmail.com>
2023-05-17 23:18:32 -0300

    [W] Working on MainActivity

--- app/src/main/java/com/quizapp/feature_quiz/presentation/MainActivity.kt
+++ app/src/main/java/com/quizapp/feature_quiz/presentation/MainActivity.kt
@@ -3,14 +3,60 @@ package com.quizapp.feature_quiz.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.quizapp.theme.CleanArchitectureNoteAppTheme
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.quizapp.feature_quiz.presentation.util.Screen
+import com.quizapp.feature_quiz.presentation.add_edit_question.components.AddEditQuestionScreen
+import com.quizapp.theme.QuizAppAndroid
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CleanArchitectureNoteAppTheme {
-                
+            QuizAppAndroid() {
+                Surface(color = MaterialTheme.colors.background)
+                {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.QuestionsScreen.route
+                    )
+                    {
+                        composable(route = Screen.QuestionsScreen.route){
+                            QuestionsScreen(navController = navController)
+                        }
+                        composable(route = Screen.AddEditQuestionScreen.route + "?questionId={questionId}&questionCategory={questionCategory}&questionDifficulty={questionDifficulty}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                )
+                                {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                )
+                                {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                }
+                            )
+                        ){
+                            val color = it.arguments?.getInt("noteColor")?: -1
+                            AddEditQuestionScreen(navController = navController, noteColor = color)
+                        }
+                    }
+                }
             }
         }
     }
